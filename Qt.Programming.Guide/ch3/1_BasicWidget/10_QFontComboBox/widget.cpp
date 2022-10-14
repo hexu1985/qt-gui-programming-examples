b74a1e03af2a5695f8261a1e62059b3c66790df8
hexu <hexu_1985@sina.com>
2022-10-14 11:13:31 +0800

    adjust Qt.Programming.Guide/ch3/1_BasicWidget/10_QFontComboBox/widget.cpp indent

--- Qt.Programming.Guide/ch3/1_BasicWidget/10_QFontComboBox/widget.cpp
+++ Qt.Programming.Guide/ch3/1_BasicWidget/10_QFontComboBox/widget.cpp
@@ -4,32 +4,32 @@ Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
 
-QFontComboBox *fontcombo[5];
+    QFontComboBox *fontcombo[5];
 
-for(int i = 0 ; i < 5 ; i++)
-    fontcombo[i] = new QFontComboBox(this);
+    for(int i = 0 ; i < 5 ; i++)
+        fontcombo[i] = new QFontComboBox(this);
 
-fontcombo[0]->setFontFilters(QFontComboBox::AllFonts);
-fontcombo[1]->setFontFilters(QFontComboBox::ScalableFonts);
-fontcombo[2]->setFontFilters(QFontComboBox::NonScalableFonts);
-fontcombo[3]->setFontFilters(QFontComboBox::MonospacedFonts);
-fontcombo[4]->setFontFilters(QFontComboBox::ProportionalFonts);
+    fontcombo[0]->setFontFilters(QFontComboBox::AllFonts);
+    fontcombo[1]->setFontFilters(QFontComboBox::ScalableFonts);
+    fontcombo[2]->setFontFilters(QFontComboBox::NonScalableFonts);
+    fontcombo[3]->setFontFilters(QFontComboBox::MonospacedFonts);
+    fontcombo[4]->setFontFilters(QFontComboBox::ProportionalFonts);
 
-int ypos = 30;
-for(int i = 0 ; i < 5 ; i++)
-{
-    fontcombo[i]->setGeometry(10, ypos, 200, 30);
-    ypos += 40;
-}
+    int ypos = 30;
+    for(int i = 0 ; i < 5 ; i++)
+    {
+        fontcombo[i]->setGeometry(10, ypos, 200, 30);
+        ypos += 40;
+    }
 
-lbl = new QLabel("I love Qt programming", this);
-lbl->setGeometry(10, ypos, 200, 30);
+    lbl = new QLabel("I love Qt programming", this);
+    lbl->setGeometry(10, ypos, 200, 30);
 
-connect(fontcombo[0], SIGNAL(currentIndexChanged(int)), this,
-                      SLOT(changedIndex(int)));
+    connect(fontcombo[0], SIGNAL(currentIndexChanged(int)), this,
+            SLOT(changedIndex(int)));
 
-connect(fontcombo[0], SIGNAL(currentFontChanged(QFont)), this,
-                      SLOT(changedFont(const QFont &)));
+    connect(fontcombo[0], SIGNAL(currentFontChanged(QFont)), this,
+            SLOT(changedFont(const QFont &)));
 }
 
 void Widget::changedIndex(int idx)
